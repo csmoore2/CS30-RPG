15ab83a688107fd3ee91c5ec93ea8c0300897e0c
Christopher Moore <csmoore001@gmail.com>
2022-04-19 09:13:10 -0600

    Create Project

---
+++ RPG Project/src/game/Main.java
@@ -0,0 +1,16 @@
+package game;
+
+/**
+ * This class contains the entry point into the game; it is where
+ * the initial setup takes place and the game starts.
+ */
+public class Main {
+	/**
+	 * This is the entry point into the program.
+	 * 
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
